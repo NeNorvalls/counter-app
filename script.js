4a95fbf2636b0b9bb08314e9d96f5bbea90edf90
NeNorvalls <honeygeeh27@gmail.com>
2024-03-15 16:33:55 +0100

    adding styles

---
+++ script.js
@@ -0,0 +1,34 @@
+// document.getElementById("count-el").innerText = 5;
+
+// let myAge = 43;
+// console.log(myAge);
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+// let myAge = 43;
+// let humanDogRatio = 7;
+
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// let count = 5;
+
+// //count + 1
+// count = count + 2;
+
+// console.log(count);
+
+// let bonusPoints = 50;
+
+// bonusPoints = bonusPoints + 50;
+// bonusPoints = bonusPoints - 75;
+// bonusPoints = bonusPoints + 45;
+
+// console.log(bonusPoints);
+
+document.getElementById("increment-btn");
